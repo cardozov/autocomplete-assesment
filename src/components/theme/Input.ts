6b4f65ecd3bfba40af444932246f70688f30d93c
cardozov <vinicius.gcardozo@gmail.com>
2024-01-07 21:26:27 -0300

    feat: :sparkles: add Input component
    - create and style Input

---
+++ src/components/theme/Input.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const Input = styled.input`
+  height: 2rem;
+  border: 0.1rem solid rgba(0, 0, 0, 0.2);
+  border-radius: 0.5rem;
+  font-size: 1rem;
+  padding: 0.2rem 1rem;
+`
+
+export default Input
